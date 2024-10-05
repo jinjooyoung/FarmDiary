d27bbf035c12a8c28dbf04e76212e90f7a2d1732
jinjooyoung <jjy26297449@gmail.com>
2024-10-05 22:22:26 +0900

    [Feat] Grid UI

--- Assets/Scripts/GameCoreSystems/GridSystems/GridData.cs
+++ Assets/Scripts/GameCoreSystems/GridSystems/GridData.cs
@@ -27,6 +27,7 @@ public class GridData
 
     private List<Vector3Int> CalculatePositions(Vector3Int gridPosition, Vector2Int objectSize)
     {
+
         List<Vector3Int> returnVal = new();
         for (int x = 0; x < objectSize.x; x++)
         {
@@ -43,8 +44,10 @@ public class GridData
         List<Vector3Int> positionToOccupy = CalculatePositions(gridPosition, objectSize);
         foreach (var pos in positionToOccupy)
         {
+            Debug.Log($"Checking position: {pos}");
             if (placedObjects.ContainsKey(pos))
             {
+                Debug.Log($"Cannot place at: {pos}");
                 return false;
             }
         }
