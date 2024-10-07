076d345fc5915b16410ad3340f5a799cc3517a4c
namsuhan0322 <rnatpakfl@gmail.com>
2024-10-07 17:57:47 +0900

    [Fix] 빌드 후 밭 클릭 안되는 문제 수정

--- Assets/Scripts/Farm/FarmFieldClickHandler.cs
+++ Assets/Scripts/Farm/FarmFieldClickHandler.cs
@@ -20,8 +20,8 @@ public class FarmFieldClickHandler : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))  // 좌클릭 체크
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
 
             if (hit.collider != null)
             {
