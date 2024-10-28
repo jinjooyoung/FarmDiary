c97e9ef29528aebde011ce2012437187a20ce268
jinjooyoung <jjy26297449@gmail.com>
2024-10-29 00:07:24 +0900

    [Fix] 작물 오브젝트를 밭 오브젝트의 하위로
    근데 이 상태에서 밭을 삭제하면 작물과 같이 삭제되긴 하지만 방금 고친 버그와 마찬가지로 정보는 남아있는 상태가 됨. 수정필요함 일단 여기까지만 푸시해둠

--- Assets/Scripts/Farm/CropPlacementState.cs
+++ Assets/Scripts/Farm/CropPlacementState.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CropPlacementState : IBuildingState
 {
@@ -51,10 +53,25 @@ public class CropPlacementState : IBuildingState
             return;
         }
 
+        // 그리드 위치에 존재하는 밭 오브젝트를 가져옴
+        GameObject fieldObject = GetFieldObjectAt(gridPosition);
+
+        if (fieldObject == null)
+        {
+            Debug.LogWarning("해당 위치에 밭 오브젝트가 없습니다.");
+            return;
+        }
+
         int index = cropPlacer.PlaceObject(database.objectsData[selectedCropIndex].Prefab, grid.CellToWorld(gridPosition));
 
         // CropGrowthManager에 등록
         GameObject placedCrop = cropPlacer.placedGameObjects[index];    // 설치된 오브젝트 가져오기
+
+        if (placedCrop != null)
+        {
+            placedCrop.transform.SetParent(fieldObject.transform);
+        }
+
         Crop cropScript = placedCrop.GetComponent<Crop>();              // Crop 스크립트 가져오기
 
         if (cropScript != null)
@@ -72,6 +89,45 @@ public class CropPlacementState : IBuildingState
             database.objectsData[selectedCropIndex].ID, index);
 
         previewSystem.UpdatePreviewOBJPos(grid.CellToWorld(gridPosition), false);
+    }
+
+    // 그리드 좌표에 밭 오브젝트가 있을 경우 그 오브젝트를 반환하는 메서드
+    public GameObject GetFieldObjectAt(Vector3Int gridPosition)
+    {
+        if (placedCropData.placedFields.TryGetValue(gridPosition, out PlacementData placement))
+        {
+            int objIndex = placement.PlacedObjectIndex;
+
+            // 인덱스가 유효한 경우 OBJList에서 오브젝트를 반환
+            if (objIndex > -1 && objIndex < cropPlacer.placedGameObjects.Count)
+            {
+                return cropPlacer.placedGameObjects[objIndex];
+            }
+        }
+
+        return null; // 위치에 밭 오브젝트가 없거나 유효하지 않으면 null 반환
+    }
+
+    // 그리드 좌표에 존재하는 오브젝트가 차지하는 모든 그리드 좌표 리스트를 반환하는 메서드
+    // 인데 사용하지는 않을 것 같음 근데 나중에 필요할수도 있을 것 같아서 일단 냅둠
+    private List<Vector3Int> GetFieldPositions(Vector3Int gridPosition)
+    {
+        if (placedCropData.placedFields.ContainsKey(gridPosition))
+        {
+            PlacementData placementData = placedCropData.placedFields[gridPosition];
+
+            if (placementData == null)
+            {
+                Debug.LogError("PlacementData is null!");
+                return null;
+            }
+
+            List<Vector3Int> positions = placementData.occupiedPositions;
+
+            return positions;
+        }
+
+        return null;
     }
 
     private bool IsFieldPlaced(Vector3Int gridPosition) // 해당 위치에 밭이 존재하면 true
