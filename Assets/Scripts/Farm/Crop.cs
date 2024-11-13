6677ab7231e1fb971e751afa850e6cf59f66a36f
jinjooyoung <jjy26297449@gmail.com>
2024-11-13 23:22:41 +0900

    [Feat] 나무기능 완료 (리소스 받으면 테스트 해봐야함)
    Crop 스크립트 붙인 프리팹 만들 때 성목 -> 열매 리소스 배열 넣어주고, ID 100 이상으로, 열매 열리는 시간 float 3개 배열에 시간 할당 해줘야함

--- Assets/Scripts/Farm/Crop.cs
+++ Assets/Scripts/Farm/Crop.cs
@@ -33,10 +33,10 @@ public class Crop : MonoBehaviour
     public bool isPreview;
 
     public GameObject[] growthStages; // 성장 단계별로 할당된 리소스 오브젝트들 (총 5개)
-    public bool isTree = false;         // 나무인가?
+    private bool isTree = false;         // 나무인가?
     public GameObject[] fruitStages;    // 나무일때 필요한 성목 -> 열매 리소스 오브젝트들
     private float[] growthTimes;  // 각 성장 단계에 필요한 시간
-    private float[] fruitTimes;   // 열매가 열리기까지 필요한 시간
+    public float[] fruitTimes = new float[3];   // 열매가 열리기까지 필요한 시간
     public int currentStage = 0;  // 현재 작물의 성장 단계
     private int currentFruitStage = 0;  // 현재 작물의 열매 성장 단계
     private float growthStartTime; // 성장이 시작된 시간
@@ -166,9 +166,16 @@ public class Crop : MonoBehaviour
             cropState = CropState.Harvested;
             GameManager.AddCoins(sellPrice);
             aiStateManager.AddToInventory(this);
-            if (ID < 100)   // 나무가 아니라면
+            if (ID >= 100)   // 나무라면 1차 수확 이후 열매 루틴이 돌아야함
             {
-                gameObject.SetActive(false);
+                isTree = true;
+                cropState = CropState.Watered;
+                InitializeFruit();  // 나무 열매 루틴 초기 설정
+            }
+            else    // 그냥 작물이라면
+            {
+                gameObject.SetActive(false);        // 오브젝트를 비활성화
+                // 근데 여기 비활성화 해둔건 수확할때 저장하는 리스트에서 삭제돼서 Missing이 떠서 비활성화로 해둔거임 이 문제 수정하고 Destroy로 변경해야함
             }
         }
         else
@@ -184,37 +191,32 @@ public class Crop : MonoBehaviour
         growthStartTime = Time.time;  // 성장이 시작된 시간을 저장
         currentStage = 0;             // 초기 성장 단계 설정
 
-        UpdateCropVisual();            // 초기 상태 업데이트
         UpdateSortingLayer();          // 초기 소팅 레이어 업데이트
+        UpdateCropVisual();            // 초기 상태 업데이트
         growthStages[5].SetActive(false);   // 물 텍스쳐 처음에는 꺼짐
     }
 
     // 위 메서드의 나무 버전
-    public void InitializeFruit(float[] cropfruitTimes)
+    public void InitializeFruit()
     {
-        fruitTimes = cropfruitTimes;
         growthStartTime = Time.time;  // 성장이 시작된 시간을 저장
         currentFruitStage = 0;             // 초기 성장 단계 설정
 
+        UpdateSortingLayerTree();          // 초기 소팅 레이어 업데이트
         UpdateTreeVisual();            // 초기 상태 업데이트
-        UpdateSortingLayer();          // 초기 소팅 레이어 업데이트
     }
 
     // 각 단계별로 성장을 체크하고 성장 상태를 업데이트
     // CropGrowthManager 스크립트에서 호출됨
     public void CheckGrowth(float currentTime)
     {
-        if (ID > 100)   // 나무라면
-        {
-            
-        }
         // 수확된 경우에는 아무 작업도 하지 않음
-        if (cropState == CropState.ReadyToHarvest)
+        if (cropState == CropState.ReadyToHarvest)      // 수확 대기상태
         {
             return;
         }
 
-        if (cropState == CropState.Harvested)
+        if (cropState == CropState.Harvested)           // 수확됨 상태
         {
             return;
         }
@@ -226,12 +228,29 @@ public class Crop : MonoBehaviour
             return; // 물을 줄 때까지 성장 멈춤
         }
 
+        if (isTree)     // 나무 열매 수확 1번 이상 했을 경우
+        {
+            // 물을 준 이후 성장 단계가 1 이상으로 진행되도록 설정
+            if (currentFruitStage < fruitStages.Length && currentTime - growthStartTime >= fruitTimes[currentFruitStage])
+            {
+                currentFruitStage++;
+                UpdateTreeVisual();
+                Debug.Log($"현재 성장 단계: {currentFruitStage}");
+
+                // 성장 단계가 마지막 단계인 경우 ReadyToHarvest 상태로 설정
+                if (currentFruitStage == 2)  // 배열 마지막 단계 확인
+                {
+                    cropState = CropState.ReadyToHarvest;
+                    Debug.Log("작물이 다 자라서 수확할 준비가 되었습니다.");
+                }
+            }
+        }
+
         // 물을 준 이후 성장 단계가 1 이상으로 진행되도록 설정
         if (currentStage < growthStages.Length && currentTime - growthStartTime >= growthTimes[currentStage])
         {
             currentStage++;
             UpdateCropVisual();
-            UpdateSortingLayer();
             Debug.Log($"현재 성장 단계: {currentStage}");
 
             // 성장 단계가 마지막 단계인 경우 ReadyToHarvest 상태로 설정
@@ -281,6 +300,19 @@ public class Crop : MonoBehaviour
         for (int i = 0; i < 5; i++)
         {
             SpriteRenderer renderer = growthStages[i].GetComponent<SpriteRenderer>();
+            if (renderer != null)
+            {
+                renderer.sortingLayerName = "MiddleGround"; // 원하는 소팅 레이어 이름으로 변경
+                renderer.sortingOrder = CalculateSortingOrder(); // 계산된 소팅 오더로 설정
+            }
+        }
+    }
+
+    private void UpdateSortingLayerTree()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            SpriteRenderer renderer = fruitStages[i].GetComponent<SpriteRenderer>();
             if (renderer != null)
             {
                 renderer.sortingLayerName = "MiddleGround"; // 원하는 소팅 레이어 이름으로 변경
